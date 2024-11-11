5ef3a10d37369a01677d293083a8b8e8d58ad58d
Barsan Patricia <barsanpatricia@gmail.com>
2024-11-11 21:55:03 +0200

    Made SignUp page run

--- frontend/Looped/app/src/main/java/msa/looped/SignUpPage.java
+++ frontend/Looped/app/src/main/java/msa/looped/SignUpPage.java
@@ -1,6 +1,38 @@
 package msa.looped;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import msa.looped.databinding.SignupPageBinding;
+
+
 public class SignUpPage extends Fragment {
+
+    private SignupPageBinding binding;
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = SignupPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
